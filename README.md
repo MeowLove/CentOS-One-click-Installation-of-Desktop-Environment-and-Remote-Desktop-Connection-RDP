1d0ef8fec0e11f34286d3c16ea1bf384b73f5ca5
MeowLove <MeowLove@users.noreply.github.com>
2018-08-07 09:32:10 +0800

    Version 0.3.1
    [Version 0.3.1]
    Test, do not use, add Windows application support.

--- README.md
+++ README.md
@@ -6,30 +6,37 @@ My website: https://tech.cxthhhhh.com
 Features:
 1. Install the desktop environment and RDP remote desktop connection for your CentOS with one click.
 
-    (You need a newly installed CentOS7, the Minimum Hardware requirements are: CPU core ≥ 1, Memory ≥ 1GB, Disk space ≥ 15GB.)
+    (You need a newly installed CentOS7, the Minimum Hardware requirements are: CPU core ≥ 1, Memory ≥ 1GB, Disk space ≥ 20GB.)
 
     Recommended system : [Original] Network one-click Reinstall CentOS 7 (Official, Pure, Safe, Efficient) 
     URL : https://tech.cxthhhhh.com/linux/2018/07/30/original-network-one-click-reinstall-centos-7-official-pure-safe-efficient-en.html
 
-2. Create a 3GB Swap Memory. Avoid errors caused by insufficient memory.
+2. Windows application support. (via Wine)
+
+    (Install Wine X64 and X86, now you can Run Windows applications On Linux.)
+
+3. Create a 5GB Swap Memory. Avoid errors caused by insufficient memory.
 
     (Swap Location: /var/swapd)
 
-3. Install Chinese input environment, Chinese support.
+4. Install Chinese input environment, Chinese support.
 
     (Without a Chinese environment, you will not be able to view Chinese fonts. Will lead to coding confusion.)
 
-4. Install software that people recommend, such as browsers and input methods.
+5. Install software that people recommend, such as browsers and input methods.
 
     (I posted a survey here http://www.hostloc.com/thread-466449-1-1.html. Respect the results of the survey and Chrome is installed.)
 
 
 Let's get started (via root user)
 
-    sudo curl -sSL https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/install.sh | sudo bash
+    sudo curl -sSL https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/main/install.sh | sudo bash
+
+
 
 
 Now that you have done it, you can connect via RDP (IP: 3389)
+
 
 You should check out my tutorial to find out what you need to pay attention to.
 
@@ -37,4 +44,4 @@ You should check out my tutorial to find out what you need to pay attention to.
     URL : Waiting to add
 
 
-Updated on Aug 6, 2018
+Updated on Aug 7, 2018
---
+++ download/main/install.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "Welcome to Desktop environment and Remote connection tool ! V0.3"
+echo "System requirements: CentOS 7+ New installation"
+echo "Recommended system : [Original] Network one-click Reinstall CentOS 7 (Official, Pure, Safe, Efficient)"
+echo "URL : https://tech.cxthhhhh.com/linux/2018/07/30/original-network-one-click-reinstall-centos-7-official-pure-safe-efficient-en.html"
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "from https://tech.cxthhhhh.com - 2018/08/07 - MeowLove"
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo Press any key to continue! Exit with 'Ctrl'+'C' !
+
+echo -e "\n"
+sudo cd /root
+yum install epel-release -y
+echo "Start creating Swaps Memory, please wait..."
+dd if=/dev/zero of=/var/swapd bs=1024 count=5242880
+mkswap /var/swapd
+chmod 0644 /var/swapd
+swapon /var/swapd
+echo '/var/swapd   swap   swap   default 0 0' >> /etc/fstab
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "Swaps Memory is Created  is [OK]"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+echo -e "\n"
+sudo cd /root
+yum groupinstall "X Window System" -y
+yum groupinstall "MATE Desktop" -y
+yum remove firefox -y
+yum groupinstall "Chinese support" -y
+yum groups mark install "Chinese support" -y
+yum install cjkuni-ukai-fonts cjkuni-uming-fonts -y
+yum install fontforge -y
+yum install ibus-libpinyin im-chooser -y
+localectl set-locale LANG=zh_CN.UTF8
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "Desktop environment is installed  is [OK]"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+echo -e "\n"
+sudo cd /root
+touch /etc/yum.repos.d/google-chrome.repo
+echo '[google-chrome]
+name=google-chrome
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.google.com/linux/linux_signing_key.pub'>/etc/yum.repos.d/google-chrome.repo
+echo -e "\n"
+cat /etc/yum.repos.d/google-chrome.repo
+yum install google-chrome-stable -y
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "The Chrome Already installed  is [OK]"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+echo -e "\n"
+sudo cd /root
+yum install https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/wine/wine32-release.rpm -y
+yum install wine.i686 -y
+yum install wine -y
+sudo wget -O /usr/share/wine/fonts/msyh.ttc https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/msyh.ttc
+sudo wget -O /usr/share/wine/fonts/msyhbd.ttc https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/msyhbd.ttc
+sudo wget -O /usr/share/wine/fonts/msyhl.ttc https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/msyhl.ttc
+sudo wget -O /usr/share/wine/fonts/simfang.ttf https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/simfang.ttf
+sudo wget -O /usr/share/wine/fonts/simhei.ttf https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/simhei.ttf
+sudo wget -O /usr/share/wine/fonts/simkai.ttf https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/simkai.ttf
+sudo wget -O /usr/share/wine/fonts/SIMLI.ttf https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/SIMLI.ttf
+sudo wget -O /usr/share/wine/fonts/simsun.ttc https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/simsun.ttc
+sudo wget -O /usr/share/wine/fonts/SIMYOU.ttf https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/SIMYOU.ttf
+sudo wget -O /usr/share/wine/fonts/仿宋_GB2312.ttf https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/仿宋_GB2312.ttf
+sudo wget -O /usr/share/wine/fonts/楷体_GB2312.ttf https://raw.githubusercontent.com/MeowLove/CentOS-One-click-Installation-of-Desktop-Environment-and-Remote-Desktop-Connection-RDP/master/download/ttf/楷体_GB2312.ttf
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "The Wine Already installed  is [OK]"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+echo -e "\n"
+sudo cd /root
+yum install tigervnc-server tigervnc -y
+yum install xrdp -y
+touch ~/.Xclients
+echo "mate-session" > ~/.Xclients
+chmod a+x ~/.Xclients
+systemctl enable xrdp
+systemctl start xrdp
+firewall-cmd --add-port=3389/tcp --permanent
+firewall-cmd --reload
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "Remote connection RDP is installed  is [OK]"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+echo -e "\n\n\n"
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "The current default system language is Chinese."
+echo "If you are an English user, please execute ' sudo localectl set-locale LANG=en_US.UTF-8 '"
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "Warning: Now, you need to pay attention to the following, please visit this page."
+echo "Technical Blog | 技術博客 | 易，窮則變，變則通，通則久。是以自天佑之，吉無不利"
+echo "https://tech.cxthhhhh.com"
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "End to Desktop environment and Remote connection tool ! V0.3"
+echo "Everything is ready and the system is restarting. Then you can connect via (RDP)IP:3389."
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "from https://tech.cxthhhhh.com - 2018/08/07 - MeowLove"
+echo "---------------------------------------------------------------------------------------------------------------------"
+reboot !
+echo -e "\n"
